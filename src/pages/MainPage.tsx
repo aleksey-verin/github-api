50314710326786af6fe988af30db2637261cbeca
Aleksey Verin <verevaa@yandex.ru>
2023-05-24 20:50:42 +0300

    feat: started doing a search on external repos

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,19 +1,41 @@
-import { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import MainContent from '../components/ui/MainContent';
 import RepoItem from '../components/search/RepoItem';
 import { useSelector } from 'react-redux';
 import { selectorUserSlice } from '../store/reducers/userSlice';
+import useDebounce from '../hooks/useDebounce';
+
+const defaultValue = '';
 
 interface MainPageProps {}
 
 const MainPage: FC<MainPageProps> = () => {
   const { userRepos, isLoading, isError } = useSelector(selectorUserSlice);
 
+  const [searchValue, setSearchValue] = useState<string>(defaultValue);
+  const debouncedValue = useDebounce<string>(searchValue, 2000);
+
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const search = e.target.value;
+    setSearchValue(search);
+  };
+
+  useEffect(() => {
+    if (!searchValue) return;
+    console.log(searchValue);
+  }, [debouncedValue]);
+
   return (
     <MainContent>
       <form className="search">
         <label htmlFor="search">Search through all repositories on github:</label>
-        <input id="search" type="text" placeholder="Enter request.." />
+        <input
+          value={searchValue}
+          id="search"
+          type="text"
+          placeholder="Enter request.."
+          onChange={handleInputValue}
+        />
       </form>
       <section className="user-repositories">
         <div className="user-repositories__title">List of your repositories:</div>
