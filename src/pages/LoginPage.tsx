ab59edb15b0817c9a5e98c5540a8fa712fbc3034
Aleksey Verin <verevaa@yandex.ru>
2023-05-25 12:22:32 +0300

    fix: some styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,28 @@ import MainContent from '../components/ui/MainContent';
 interface LoginPageProps {}
 
 const LoginPage: FC<LoginPageProps> = () => {
-  return <MainContent>LoginPage</MainContent>;
+  // const handleAuth = async () => {
+  //   try {
+  //     const response = await fetch('https://github.com/login/oauth/authorize?scope=user:e');
+  //   if (response.ok) {
+  //     const data = await response.json();
+  //     console.log(data);
+  //     return data;
+  //   } else {
+  //     const error = await response.json();
+  //     console.log(error);
+  //   }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  return (
+    <MainContent>
+      <div>Auth with GitHub</div>
+      <button>Click</button>
+    </MainContent>
+  );
 };
 
 export default LoginPage;
